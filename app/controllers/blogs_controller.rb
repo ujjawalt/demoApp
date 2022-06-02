dd9532ef1c1cd4eb0f8904ea7c2e04bfb35f8928
ujjawalt <tanwar896@outlook.com>
2022-06-02 15:59:08 +0530

    Edit Page & update: route, action & view

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,11 +12,26 @@ class BlogsController < ApplicationController
         @blog = Blog.new
     end
 
+    def edit
+        @blog = Blog.find(params[:id])
+    end
+
     def create
         @blog = Blog.new(params.require(:blog).permit(:title, :description))
         if
             @blog.save
             flash[:notice]="Blog created successfully"
+            redirect_to @blog
+        else
+            render "new"
+        end
+    end
+
+    def update
+        @blog = Blog.find(params[:id])
+        if
+            @blog.update(params.require(:blog).permit(:title, :description))
+            flash[:notice]="Blog updated successfully"
             redirect_to @blog
         else
             render "new"
